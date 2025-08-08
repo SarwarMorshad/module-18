31049fd297bf6c1509317ecb60a99adfd4085ac6
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-08-08 10:33:48 +0200

    Array Traversing

--- array.js
+++ array.js
@@ -1,5 +1,20 @@
 const fruits = ["apple", "banana", "cherry"];
 
 for (let i = 0; i < fruits.length; i++) {
-  console.log(fruits[i]);
+  //   console.log(fruits[i]);
 }
+
+for (const fruit of fruits) {
+  //   console.log(fruit);
+}
+
+// array reversal
+// const reversedFruits = fruits.slice().reverse();
+// console.log(reversedFruits);
+
+// array reverse using for loop
+const reversedFruits = [];
+for (let i = 0; i < fruits.length; i++) {
+  reversedFruits.unshift(fruits[i]);
+}
+console.log(reversedFruits);
